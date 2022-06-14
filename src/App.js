fa8d2f2afb49ed5d935abc9779decd2e6ff1fe6b
JpTanaka <jpao.com@gmail.com>
2022-06-14 16:10:30 +0200

    added nextgen function

--- src/App.js
+++ src/App.js
@@ -23,20 +23,10 @@ function Row(props) {
   )
 }
 
-function Board() {
-  function handleClick (matrixcollumnnumber, matrixrownumber) {
-      let temp_matrix = [...boardmatrix];
-      temp_matrix[matrixrownumber][matrixcollumnnumber] = 1;
-      
-      setMatrix(temp_matrix);
-      console.log(boardmatrix);
-  }
-  const nbhorizontalsquares = 50;
-  const nbverticalsquares = 40;
-  let i = 0;
-  const [boardmatrix, setMatrix] = useState(Array(nbverticalsquares).fill().map(()=> Array(nbhorizontalsquares).fill(0)));
-  const rows = boardmatrix.map((row, index) =>
-    < Row click = {handleClick} key = {row.id} matrixrow = {row} matrixrownumber = {index}/>
+function Board(props) {
+  
+  const rows = props.boardmatrix.map((row, index) =>
+    < Row click = {props.handleClick} key = {row.id} matrixrow = {row} matrixrownumber = {index}/>
   );
   return (
     
@@ -50,26 +40,28 @@ function Board() {
 }
 
 function BeginButton(props) {
+  const phrase = (props.toggle? "Stop":"Begin");
   return (
     <button class="button-30 begin" role="button" onClick={props.isToggle}>
-      Begin
+      {phrase}
     </button>
 
     )
 }
 
-function Counter(props) {
+// function Counter(props) {
   
-  const counterphrase = "Counter: " + props.counter + (props.toggle? " true":" false")
-  // 
-  return (
-    <div>
-    {counterphrase}
-    </div>
-  )
-}
+//   const counterphrase = "Counter: " + props.counter + (props.toggle? " true":" false")
+//   // 
+//   return (
+//     <div>
+//     {counterphrase}
+//     </div>
+//   )
+// }
 
 function App() {
+  
   const [counter, setCounter] = useState(0)
   const [toggle, setToggle] = useState(false)
   function isToggle() {
@@ -79,20 +71,56 @@ function App() {
     if (toggle) {
         const interval = setInterval(() => {
             setCounter(counter=>counter+1);
-
-        }, 1000)  
+            setMatrix(boardmatrix=>nextGen(boardmatrix));
+        }, 500)  
         return () => clearInterval(interval);}
     }
 , [toggle])
+
+
+function nextGen(cells) {
+  var get = function (i, j) { return (cells[i] && cells[i][j]) | 0 };
   
+  cells = cells.map(function (row, i) {
+    return row.map(function (alive, j) {
+      var neighbors =
+        get(i-1, j-1) + get(i-1, j) + get(i-1, j+1) +
+        get(i  , j-1)               + get(i  , j+1) +
+        get(i+1, j-1) + get(i+1, j) + get(i+1, j+1);
+        
+      return (neighbors === 3 || (neighbors === 2 && alive)) | 0;
+    });
+  });
+  
+  return cells;
+}
+
+
+function handleClick (matrixcollumnnumber, matrixrownumber) {
+  let temp_matrix = [...boardmatrix];
+  temp_matrix[matrixrownumber][matrixcollumnnumber] = (temp_matrix[matrixrownumber][matrixcollumnnumber] ? 0 : 1);
+  
+  setMatrix(temp_matrix);
+  console.log(boardmatrix);
+}
+const nbhorizontalsquares = 50;
+const nbverticalsquares = 40;
+let i = 0;
+const [boardmatrix, setMatrix] = useState(Array(nbverticalsquares).fill().map(()=> Array(nbhorizontalsquares).fill(0)));
+
+
 //
   return (
     <>
     <div className="container">
-      {counter}
-      < Counter counter = {counter} setCounter = {setCounter} toggle = {toggle} />
-    < BeginButton isToggle = {isToggle} />  
-    < Board />
+      <div className="title">
+        <p className="title-game">Conway's Game of 
+        {/* <span className='title-game life'> Life</span> */}
+        </p>
+      </div>
+      {/* < Counter counter = {counter} setCounter = {setCounter} toggle = {toggle} /> */}
+    < BeginButton isToggle = {isToggle} toggle = {toggle} />  
+    < Board handleClick={handleClick} boardmatrix={boardmatrix} setMatrix={setMatrix} />
     
     </div>
 
